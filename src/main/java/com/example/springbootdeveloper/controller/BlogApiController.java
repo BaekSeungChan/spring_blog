8c950189fe9de95db3c4550739376873090fd43c
baekseungchan1117 <bsc7386@gmail.com>
2024-01-29 12:35:00 +0900

    BlogApiController

---
+++ src/main/java/com/example/springbootdeveloper/controller/BlogApiController.java
@@ -0,0 +1,30 @@
+package com.example.springbootdeveloper.controller;
+
+import com.example.springbootdeveloper.domain.Article;
+import com.example.springbootdeveloper.dto.AddArticleRequest;
+import com.example.springbootdeveloper.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
+@RequestMapping("/api")
+public class BlogApiController{
+
+    private final BlogService blogService;
+
+    // HTTP 메서드가 POST일 때 전달 받은 URL과 동일하면 메서드로 매핑
+    @PostMapping("/articles")
+    // @RequestBody로 요청 본문 값 매핑
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
+        Article saveArticle = blogService.save(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(saveArticle);
+    }
+}
