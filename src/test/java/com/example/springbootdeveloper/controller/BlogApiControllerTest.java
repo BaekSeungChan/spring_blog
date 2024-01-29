90e809e71d92afe70922d3408cbc2afdd7a90533
baekseungchan1117 <bsc7386@gmail.com>
2024-01-29 20:49:42 +0900

    테스트 코드 주석 추가

--- src/test/java/com/example/springbootdeveloper/controller/BlogApiControllerTest.java
+++ src/test/java/com/example/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -1,17 +1,28 @@
 package com.example.springbootdeveloper.controller;
 
+import com.example.springbootdeveloper.domain.Article;
+import com.example.springbootdeveloper.dto.AddArticleRequest;
 import com.example.springbootdeveloper.repository.BlogRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest // 테스트용 애플리케이션 컨텍스트
 @AutoConfigureMockMvc // MockMvc 생성 및 자동 구성
@@ -41,5 +52,35 @@ class BlogApiControllerTest {
     //context 변수가 MockMvcBuilders.webAppContextSetup 메서드에 전달되는 이유는, 테스트 환경을 실제 애플리케이션 컨텍스트와 일치시키기 위함입니다.
     // 이렇게 함으로써, 테스트는 실제 애플리케이션과 동일한 환경에서 실행되며, 실제 애플리케이션의 설정, 빈(Bean) 구성, MVC 설정 등을 그대로 사용할 수 있습니다.
 
+    @DisplayName("addArticle: 블로그 글 추가에 성공한다.")
+    @Test
+    public void addArticle() throws Exception {
+        // given
+        final String url = "/api/articles";
+        final String title = "title";
+        final String content = "content";
+        final AddArticleRequest userRequest = new AddArticleRequest(title, content);
 
+        // 객체 JSON으로 직렬화
+        final String requestBody = objectMapper.writeValueAsString(userRequest);
+
+        //When
+        // 설정한 내용을 바탕으로 요청 전송
+        ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestBody));
+
+        //then
+        result.andExpect(status().isCreated());
+
+        List<Article> articles = blogRepository.findAll();
+
+        assertThat(articles.size()).isEqualTo(1);
+        assertThat(articles.get(0).getTitle()).isEqualTo(title);
+        assertThat(articles.get(0).getContent()).isEqualTo(content);
+    }
+
+    // writeValueAsString() 메서드를 사용해서 객체를 JSON으로 직렬화해줍니다. 그 이후에는 MockMvc를 사용해 HTTP 메서드, URL, 요청 본문, 요청 타입 등을 설정한 뒤
+    // 설정한 내용을 바탕으로 테스트 요청을 보냅니다. contentType() 메서드는 요청을 보낼 때 JSON< XML 등 다양한 타입 중 하나를 골라 요청을 보냅니다. 여기에서 JSON 타입의 요청을 보낸다고 명시했습니다.
+    // assertThat() 메서드로는 블로그 글ㄹ의 개수가 1인지 확인합니다.
 }
