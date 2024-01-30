c7057980e5a54777a1bf30017d9298364415cb33
baekseungchan1117 <bsc7386@gmail.com>
2024-01-30 14:23:38 +0900

    상세 조회 테스트코드

--- src/test/java/com/example/springbootdeveloper/controller/BlogApiControllerTest.java
+++ src/test/java/com/example/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -112,4 +112,28 @@ class BlogApiControllerTest {
                 .andExpect(jsonPath("$[0].title").value(title));
     }
 
+
+    @DisplayName("findArticle: 블로그 글 조회에 성공한다.")
+    @Test
+    public void findDetailArticle() throws Exception{
+        // given
+        final String url = "/api/articles/{id}";
+        final String title = "title";
+        final String content = "content";
+
+        Article saveArticle =blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(get(url, saveArticle.getId()));
+
+        // then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value(content))
+                .andExpect(jsonPath("$.title").value(title));
+    }
+
 }
