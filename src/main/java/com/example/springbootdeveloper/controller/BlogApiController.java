0aec21dcc75cebefda3e748b507e85498d5bfc98
baekseungchan1117 <bsc7386@gmail.com>
2024-01-30 11:01:05 +0900

    findAll Controller

--- src/main/java/com/example/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/com/example/springbootdeveloper/controller/BlogApiController.java
@@ -2,14 +2,14 @@ package com.example.springbootdeveloper.controller;
 
 import com.example.springbootdeveloper.domain.Article;
 import com.example.springbootdeveloper.dto.AddArticleRequest;
+import com.example.springbootdeveloper.dto.ArticleResponse;
 import com.example.springbootdeveloper.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
@@ -26,5 +26,16 @@ public class BlogApiController{
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(saveArticle);
+    }
+
+    @GetMapping("/articles/get")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
