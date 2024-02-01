a73d72fbeac9a361a697fb2bd88d1dd5f497f674
baekseungchan1117 <bsc7386@gmail.com>
2024-02-01 10:24:44 +0900

    블로그 글 목룍 뷰 구현하기

---
+++ src/main/java/com/example/springbootdeveloper/controller/BlogViewcontroller.java
@@ -0,0 +1,30 @@
+package com.example.springbootdeveloper.controller;
+
+import com.example.springbootdeveloper.dto.ArticleListViewResponse;
+import com.example.springbootdeveloper.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class BlogViewcontroller {
+
+    private final BlogService blogService;
+
+    @GetMapping("articles")
+    public String getArticles(Model model){
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+
+    }
+}
