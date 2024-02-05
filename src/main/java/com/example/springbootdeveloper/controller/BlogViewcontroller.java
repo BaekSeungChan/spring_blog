47157ff62a999cf25c79296d116e36fe80e8f413
baekseungchan1117 <bsc7386@gmail.com>
2024-02-05 15:12:18 +0900

    HTML VIEW 만들기

--- src/main/java/com/example/springbootdeveloper/controller/BlogViewcontroller.java
+++ src/main/java/com/example/springbootdeveloper/controller/BlogViewcontroller.java
@@ -1,11 +1,14 @@
 package com.example.springbootdeveloper.controller;
 
+import com.example.springbootdeveloper.domain.Article;
 import com.example.springbootdeveloper.dto.ArticleListViewResponse;
+import com.example.springbootdeveloper.dto.ArticleResponse;
 import com.example.springbootdeveloper.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,7 +19,7 @@ public class BlogViewcontroller {
 
     private final BlogService blogService;
 
-    @GetMapping("articles")
+    @GetMapping("/articles")
     public String getArticles(Model model){
         List<ArticleListViewResponse> articles = blogService.findAll().stream()
                 .map(ArticleListViewResponse::new)
@@ -25,6 +28,13 @@ public class BlogViewcontroller {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
 
+    @GetMapping("/articles/{id}")
+    public String getArtcle(@PathVariable Long id, Model model){
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleResponse(article));
+
+        return "article";
     }
 }
--- src/main/java/com/example/springbootdeveloper/dto/ArticleResponse.java
+++ src/main/java/com/example/springbootdeveloper/dto/ArticleResponse.java
@@ -3,13 +3,18 @@ package com.example.springbootdeveloper.dto;
 import com.example.springbootdeveloper.domain.Article;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class ArticleResponse {
     private String title;
     private String content;
+    private LocalDateTime createAt; // 필드 추가
 
-    public ArticleResponse(Article article){
+    // 생성자에 createAt 파라미터 추가
+    public ArticleResponse(Article article) {
         this.title = article.getTitle();
         this.content = article.getContent();
+        this.createAt = article.getCreateAt(); // Article 엔티티의 createAt 값을 설정
     }
 }
