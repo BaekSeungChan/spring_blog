b864b3c894b38e92494c6de52affd00748845480
baekseungchan1117 <bsc7386@gmail.com>
2024-02-05 15:29:25 +0900

    delete 실행

--- src/main/java/com/example/springbootdeveloper/dto/ArticleResponse.java
+++ src/main/java/com/example/springbootdeveloper/dto/ArticleResponse.java
@@ -1,18 +1,21 @@
 package com.example.springbootdeveloper.dto;
 
 import com.example.springbootdeveloper.domain.Article;
+import lombok.Data;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@Getter
+@Data
 public class ArticleResponse {
+    private Long id;
     private String title;
     private String content;
     private LocalDateTime createAt; // 필드 추가
 
     // 생성자에 createAt 파라미터 추가
     public ArticleResponse(Article article) {
+        this.id = article.getId();
         this.title = article.getTitle();
         this.content = article.getContent();
         this.createAt = article.getCreateAt(); // Article 엔티티의 createAt 값을 설정
